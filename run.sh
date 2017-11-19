a43fc9279ff91423ca72d37225ff226ad6366ceb
Gandalf- <austin.voecks@gmail.com>
2017-11-18 16:47:08 -0800

    Update run.sh to start sandbox too

--- run.sh
+++ run.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-# Austin Voecks
-# run.sh
-
-run="   java -cp .:commons-math3-3.6.1.jar MNIST"
+run="java -cp .:commons-math3-3.6.1.jar "
 sample="data/mnist_train.csv 6000  data/mnist_test.csv 1000"
-full="  data/mnist_train.csv 60000 data/mnist_test.csv 10000"
+full="data/mnist_train.csv 60000 data/mnist_test.csv 10000"
 
-if [[ "$1" == "sample" ]]; then
-  echo "[run.sh] Running on sample set"
-  eval "$run" "$sample"
-  exit
-fi
+h_sample="run on sample MNIST data set"
+h_full="run on full MNIST data set"
+h_sand="start the sandbox example program"
 
-echo "[run.sh] Running on full set"
-eval "$run" "$full"
+case $1 in
+  sample)
+    eval "$run MNIST $sample"
+    ;;
+
+  sandbox)
+    eval "$run Sandbox"
+    ;;
+
+  full)
+    eval "$run MNIST $full"
+    ;;
+
+  ''|*)
+    echo "
+options
+  full      $h_full
+  sample    $h_sample
+  sandbox   $h_sand
+  "
+esac
